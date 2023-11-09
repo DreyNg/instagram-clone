72e31f606dd51ed02a7cc3a9642fbff5346a3809
DreyNg <101800183+DreyNg@users.noreply.github.com>
2023-11-09 16:24:57 +1100

    init

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+import Firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const config = {
+    apiKey: "AIzaSyBIfVE0slC4M1xhF4XdZglvASYrd83Rjbk",
+    authDomain: "instagram-clone-e91a2.firebaseapp.com",
+    projectId: "instagram-clone-e91a2",
+    storageBucket: "instagram-clone-e91a2.appspot.com",
+    messagingSenderId: "435491003222",
+    appId: "1:435491003222:web:cdbb10abde3f2d9bdd14f7",
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+export { firebase, FieldValue };
