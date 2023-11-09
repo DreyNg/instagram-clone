3cc4ea56746fc6d8f191824b465a8d09cc8f4b9b
DreyNg <101800183+DreyNg@users.noreply.github.com>
2023-11-10 01:40:38 +1100

    navigate to dashboard after click login button

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,8 @@
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import * as ROUTER from "../constants/route";
 
 export default function Login() {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
     return (
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
             <div className="flex w-3/5">
@@ -12,6 +13,9 @@ export default function Login() {
             </div>
             <div className="flex w-3/5">
                 <p>i will be the form</p>
+                <button onClick={() => navigate(ROUTER.DASHBOARD)}>
+                    I am a button
+                </button>
             </div>
         </div>
     );
