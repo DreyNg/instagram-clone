1d3598dc46a3972ffa05f982abcb846e25eea0ad
DreyNg <101800183+DreyNg@users.noreply.github.com>
2023-11-10 01:15:47 +1100

    structured login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,18 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-    const history = useNavigate();
-    return <p class="underline">This is a login Page</p>;
+    // const navigate = useNavigate();
+    return (
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+            <div className="flex w-3/5">
+                <img
+                    src="/images/iphone-with-profile.jpg"
+                    alt="iphone with Ins"
+                />
+            </div>
+            <div className="flex w-3/5">
+                <p>i will be the form</p>
+            </div>
+        </div>
+    );
 }
