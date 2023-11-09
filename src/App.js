8eddc6c80bdd12539a09e308307649e33cd0f9d7
DreyNg <101800183+DreyNg@users.noreply.github.com>
2023-11-09 19:50:06 +1100

    setup tailwind and added login.js (nothing is there yet)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import * as ROUTER from "./constants/route";
+
+const Login = lazy(() => import("./pages/login"));
+
 function App() {
-    return <p>Hello</p>;
+    return (
+        <Router>
+            <Suspense fallback={<p>Loading ......</p>}>
+                <Routes>
+                    <Route path={ROUTER.LOGIN} element={<Login />} />
+                </Routes>
+            </Suspense>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/constants/route.js
@@ -0,0 +1,5 @@
+export const LOGIN = "/login";
+export const DASHBOARD = "/";
+export const PROFILE = "/p/:username";
+export const NOTFOUND = "/not-found";
+export const SIGNUP = "/singup";
