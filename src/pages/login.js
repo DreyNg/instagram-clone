871a2417f3e9ceb408ce2dd655d0de3fcd1c9b19
DreyNg <101800183+DreyNg@users.noreply.github.com>
2023-11-11 20:26:56 +1100

    git starting point, working picture and text form

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,23 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 import * as ROUTER from "../constants/route";
 
 export default function Login() {
     const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [error, setError] = useState("");
+    const inValid = email === "" || password == "";
+
+    const handleLogin = () => {};
     return (
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
             <div className="flex w-3/5">
                 <img
-                    src="/images/iphone-with-profile.jpg"
+                    src="/images/iphone-with-profile.png"
                     alt="iphone with Ins"
                 />
             </div>
