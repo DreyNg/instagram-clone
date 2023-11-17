28f97ceeb4b87ae6e4425c54e47bbc92ea57adb0
DreyNg <101800183+DreyNg@users.noreply.github.com>
2023-11-17 12:04:08 +1100

    image URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
 import * as ROUTER from "./constants/route";
 
 const Login = lazy(() => import("./pages/Login"));
@@ -8,13 +13,26 @@ const NotFound = lazy(() => import("./pages/NotFound"));
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 
 function App() {
+    // localStorage.removeItem("currentUser");
+    const currentUser = localStorage.getItem("currentUser");
+
     return (
         <Router>
             <Suspense fallback={<p>Loading ......</p>}>
                 <Routes>
                     <Route path={ROUTER.LOGIN} element={<Login />} />
                     <Route path={ROUTER.SIGNUP} element={<SignUp />} />
-                    <Route path={ROUTER.DASHBOARD} element={<Dashboard />} />
+
+                    <Route
+                        path={ROUTER.DASHBOARD}
+                        element={
+                            currentUser ? (
+                                <Dashboard />
+                            ) : (
+                                <Navigate to={ROUTER.LOGIN} />
+                            )
+                        }
+                    />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </Suspense>
