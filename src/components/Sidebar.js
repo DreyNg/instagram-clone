5fad714c74fe16d30969089d1cd88a727047af50
lrfb212 <dreynguyen01@gmail.com>
2023-11-17 19:28:05 +1100

    refactored to UserCard

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,91 +1,40 @@
+import React from "react";
+import UserCard from "./UserCard";
+
 export default function Sidebar() {
     return (
         <div className="bg-black h-full pt-5">
             <div className="h-full mx-8 flex flex-col">
-                <div className="w-full py-2 flex items-center">
-                    <div className=" p-1 rounded-lg flex-none cursor-pointer">
-                        <img
-                            src="https://i.imgur.com/k7bSvL1.png"
-                            className="h-11 w-11 rounded-full"
-                        />
-                    </div>
-                    <div className="mx-2 flex-grow">
-                        <div className="text-white font-semibold">drey.ng</div>
-                        <div className="text-ig-grey text-sm">
-                            Drey/hung long
-                        </div>
-                    </div>
-                    <div className="flex-none text-ig-blue mx-2 text-sm font-semibold">
-                        Switch
-                    </div>
-                </div>
+                {/* Your other content */}
                 <div className="flex-1">
                     <div className="flex my-2">
                         <div className="flex-grow font-semibold text-sm text-ig-grey">
                             Suggested for you
                         </div>
-                        <div className="text-sm text-white font-semibold cursor-pointer mx-2">
+                        <div className="text-sm text-white font-semibold text-sm cursor-pointer mx-2">
                             See All
                         </div>
                     </div>
                     <div>
-                        <div className="w-full py-2 flex items-center">
-                            <div className=" p-1 rounded-lg flex-none cursor-pointer">
-                                <img
-                                    src="https://i.imgur.com/k7bSvL1.png"
-                                    className="h-11 w-11 rounded-full"
-                                />
-                            </div>
-                            <div className="mx-2 flex-grow">
-                                <div className="text-white font-semibold">
-                                    drey.ng
-                                </div>
-                                <div className="text-ig-grey text-sm">
-                                    Drey/hung long
-                                </div>
-                            </div>
-                            <div className="flex-none text-ig-blue mx-2 text-sm font-semibold">
-                                Switch
-                            </div>
-                        </div>
-                        <div className="w-full py-2 flex items-center">
-                            <div className=" p-1 rounded-lg flex-none cursor-pointer">
-                                <img
-                                    src="https://i.imgur.com/k7bSvL1.png"
-                                    className="h-11 w-11 rounded-full"
-                                />
-                            </div>
-                            <div className="mx-2 flex-grow">
-                                <div className="text-white font-semibold">
-                                    drey.ng
-                                </div>
-                                <div className="text-ig-grey text-sm">
-                                    Drey/hung long
-                                </div>
-                            </div>
-                            <div className="flex-none text-ig-blue mx-2 text-sm font-semibold">
-                                Switch
-                            </div>
-                        </div>
-                        <div className="w-full py-2 flex items-center">
-                            <div className=" p-1 rounded-lg flex-none cursor-pointer">
-                                <img
-                                    src="https://i.imgur.com/k7bSvL1.png"
-                                    className="h-11 w-11 rounded-full"
-                                />
-                            </div>
-                            <div className="mx-2 flex-grow">
-                                <div className="text-white font-semibold">
-                                    drey.ng
-                                </div>
-                                <div className="text-ig-grey text-sm">
-                                    Drey/hung long
-                                </div>
-                            </div>
-                            <div className="flex-none text-ig-blue mx-2 text-sm font-semibold">
-                                Switch
-                            </div>
-                        </div>
+                        {/* Use the UserCard component */}
+                        <UserCard
+                            avatarSrc="https://static.vecteezy.com/system/resources/previews/009/273/280/non_2x/concept-of-loneliness-and-disappointment-in-love-sad-man-sitting-element-of-the-picture-is-decorated-by-nasa-free-photo.jpg"
+                            username="abc.ng"
+                            subtitle="Followed by timothyhiew + 10"
+                            followText="Follow"
+                        />
+                        <UserCard
+                            avatarSrc="https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg"
+                            username="Ngan"
+                            subtitle="Followed by nganthropocene"
+                            followText="Follow"
+                        />
+                        <UserCard
+                            avatarSrc="https://i.guim.co.uk/img/media/63de40b99577af9b867a9c57555a432632ba760b/0_266_5616_3370/master/5616.jpg?width=1200&quality=85&auto=format&fit=max&s=59ecca4b78e0bcb76a1f47dd3ab9ead7"
+                            username="Jerome"
+                            subtitle="Followed by jerome.ngx"
+                            followText="Follow"
+                        />
                     </div>
                 </div>
             </div>
---
+++ src/components/UserCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const UserCard = ({ avatarSrc, username, subtitle, followText }) => {
+    return (
+        <div className="w-full py-2 flex items-center">
+            <div className="p-1 rounded-lg flex-none cursor-pointer">
+                <img
+                    src={avatarSrc}
+                    className="h-11 w-11 rounded-full"
+                    alt={`Avatar of ${username}`}
+                />
+            </div>
+            <div className="mx-2 flex-grow">
+                <div className="text-white font-semibold text-sm">
+                    {username}
+                </div>
+                <div className="text-ig-grey text-sm">{subtitle}</div>
+            </div>
+            <div className="flex-none text-ig-blue mx-2 text-xs font-semibold">
+                {followText}
+            </div>
+        </div>
+    );
+};
+
+export default UserCard;
