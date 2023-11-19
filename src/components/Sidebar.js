72e672961747ea0867e4e809d123e6de2c09fb7c
DreyNg <101800183+DreyNg@users.noreply.github.com>
2023-11-19 19:19:04 +1100

    getting userSuggestion from empty following

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserCard from "./UserCard";
 import { CurrentUserContext } from "../context/CurrentUserContext";
 import { useContext } from "react";
+import { getUserSuggestion } from "../services/firebase";
 
 export default function Sidebar() {
     const { currentUser } = useContext(CurrentUserContext);
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        const fetchUserSuggestions = async () => {
+            try {
+                const suggestedUsers = await getUserSuggestion(currentUser);
+                setSuggestions(suggestedUsers);
+            } catch (error) {
+                console.error("Error fetching suggestions:", error);
+            }
+        };
+
+        if (currentUser) {
+            fetchUserSuggestions();
+        }
+    }, []);
+
+    console.log(suggestions);
     return (
         <div className="h-full pt-3 pl-20">
             <div className="h-full flex flex-col">
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -28,3 +28,36 @@ export async function getUserById(uid) {
         throw new Error("uid does not exist");
     }
 }
+
+export async function getUserSuggestion(user) {
+    const following = user.following;
+    const followers = user.followers;
+
+    // follows no one:
+    if (following.length === 0) {
+        try {
+            const snapshot = await firebase
+                .firestore()
+                .collection("users")
+                .where("userId", "!=", user.userId)
+                .orderBy("userId") // Order by userId for consistent pagination
+                .limit(5)
+                .get();
+
+            const suggestedUsers = [];
+            snapshot.forEach((doc) => {
+                suggestedUsers.push(doc.data());
+            });
+            return suggestedUsers;
+        } catch (error) {
+            console.error("Error getting random users:", error);
+            return [];
+        }
+    }
+
+    // write getUserSuggestion function where it returns 5 user's userId
+    // How it works is:
+    // first grab 5 random (can be duplicate, as long as it is random, its okay, for example, if there are only 4 people in following, random 5 times in that array, can have duplicate) current userId from user.following
+    // then for each random userId, fetch one random userId from the random userId's following (can not be the curren tUserId)
+    // repeat 5 times until i have an array of 5 unique suggestion userId
+}
