9e94a46af5c99c773de2f2ea5f06927860ce7186
lrfb212 <dreynguyen01@gmail.com>
2023-11-20 18:37:51 +1100

    story UI

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -6,7 +6,7 @@ export default function Timeline() {
     const scrollLeft = () => {
         if (scrollContainerRef.current) {
             scrollContainerRef.current.scrollBy({
-                left: -200,
+                left: -800,
                 behavior: "smooth",
             });
         }
@@ -15,7 +15,7 @@ export default function Timeline() {
     const scrollRight = () => {
         if (scrollContainerRef.current) {
             scrollContainerRef.current.scrollBy({
-                left: 200,
+                left: 800,
                 behavior: "smooth",
             });
         }
@@ -23,28 +23,30 @@ export default function Timeline() {
 
     return (
         <div className="h-full">
-            <div className="bg-green-500 h-full flex flex-col">
-                <div className="bg-orange-500 relative h-24 flex items-center justify-between">
+            <div className="h-full flex flex-col ">
+                <div className="relative h-24 flex items-center justify-between mb-4 mt-4">
                     <button className="absolute left-0" onClick={scrollLeft}>
                         &lt;
                     </button>
                     <div
                         ref={scrollContainerRef}
-                        className="overflow-hidden flex scroll-container"
+                        className="overflow-hidden flex scroll-container "
                     >
                         {/* Your avatar and username components go here */}
                         {/* Example placeholders */}
                         {Array.from({ length: 15 }, (_, index) => (
                             <div
                                 key={index}
-                                className="bg-blue-500 flex justify-center items-center flex-col"
+                                className=" m-3 flex items-center flex-col"
                             >
                                 <img
-                                    className="h-11 w-11 rounded-full"
+                                    className="h-13 w-13 m-2 rounded-full"
                                     src="https://www.kidsmathgamesonline.com/images/pictures/shapes/square.jpg"
                                     alt={`avatar-${index}`}
                                 />
-                                <div>username</div>
+                                <div className="text-xs text-white">
+                                    username
+                                </div>
                             </div>
                         ))}
                         {/* ... Repeat your avatar and username components as needed */}
