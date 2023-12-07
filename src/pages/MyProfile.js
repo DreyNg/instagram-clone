6ce00e826d57aa56a00e4e91e1b8d6e09f66afca
DreyNg <101800183+DreyNg@users.noreply.github.com>
2023-12-07 22:50:18 +1100

    hover like and cmt svg

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -426,14 +426,43 @@ export default function MyProfile() {
                                             alt={`Avatar ${index}`}
                                         />
                                         <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-0 opacity-0 transition-opacity duration-300 flex items-center justify-center text-white text-center hover:bg-opacity-70 hover:opacity-100">
-                                            <div>
-                                                <p>
-                                                    Likes: {post.likes.length}
-                                                </p>
-                                                <p>
-                                                    Comments:{" "}
+                                            <div className="flex">
+                                                <div className="flex">
+                                                    <svg
+                                                        aria-label="Unlike"
+                                                        class="mr-2 "
+                                                        fill="white"
+                                                        height="22"
+                                                        role="img"
+                                                        viewBox="0 0 48 48"
+                                                        width="22"
+                                                    >
+                                                        <title>Unlike</title>
+                                                        <path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
+                                                    </svg>{" "}
+                                                    {post.likes.length}
+                                                </div>
+                                                <div className="flex">
+                                                    <svg
+                                                        aria-label="Comment"
+                                                        class="ml-6 mr-2"
+                                                        fill="white"
+                                                        height="22"
+                                                        role="img"
+                                                        viewBox="0 0 24 24"
+                                                        width="22"
+                                                    >
+                                                        <title>Comment</title>
+                                                        <path
+                                                            d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z"
+                                                            fill="white"
+                                                            stroke="white"
+                                                            stroke-linejoin="round"
+                                                            stroke-width="2"
+                                                        ></path>
+                                                    </svg>
                                                     {post.comments.length}
-                                                </p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
