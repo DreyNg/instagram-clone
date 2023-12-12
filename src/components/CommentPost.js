55519ac2979257a27a4254b904885ab63d37f7b4
DreyNg <101800183+DreyNg@users.noreply.github.com>
2023-12-13 02:28:04 +1100

    click avatar for profile

--- src/components/CommentPost.js
+++ src/components/CommentPost.js
@@ -91,11 +91,13 @@ const CommentPost = (
                         <PostAvaHasStory avatar={avatar} story={hasStory} />
                     ) : (
                         <div className="h-8 w-8 rounded-full overflow-hidden">
-                            <img
-                                src={avatar}
-                                className="w-full h-auto"
-                                alt={`Avatar of `}
-                            />
+                            <Link to={`/p/${username}`}>
+                                <img
+                                    src={avatar}
+                                    className="w-full h-auto"
+                                    alt={`Avatar of `}
+                                />
+                            </Link>
                         </div>
                     )}
                 </div>
