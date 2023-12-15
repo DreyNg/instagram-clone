0c5444ff6cdf0f8eceff29568de8f56bcb467309
DreyNg <101800183+DreyNg@users.noreply.github.com>
2023-12-15 23:40:48 +1100

    correct for save

--- src/components/Post.js
+++ src/components/Post.js
@@ -258,22 +258,18 @@ export default function Post({
                     <div>
                         <svg
                             aria-label="Save"
-                            class="cursor-pointer"
+                            class="cursor-pointer hover:fill-ig-grey"
                             fill="white"
                             height="24"
                             role="img"
-                            viewBox="0 0 24 24"
+                            viewBox="5 5 24 24"
                             width="24"
                         >
                             <title>Save</title>
-                            <polygon
-                                fill="none"
-                                points="20 21 12 13.44 4 21 4 3 20 3 20 21"
-                                stroke="white"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                            ></polygon>
+                            <path
+                                xmlns="http://www.w3.org/2000/svg"
+                                d="M23,5H9C7.346,5,6,6.346,6,8v19c0,0.382,0.218,0.73,0.561,0.898c0.344,0.167,0.752,0.126,1.053-0.109L16,21.267   l8.386,6.522C24.565,27.929,24.782,28,25,28c0.149,0,0.3-0.034,0.439-0.102C25.782,27.73,26,27.382,26,27V8C26,6.346,24.654,5,23,5   z M24,24.956l-7.386-5.745C16.434,19.07,16.217,19,16,19s-0.434,0.07-0.614,0.21L8,24.956V8c0-0.551,0.449-1,1-1h14   c0.551,0,1,0.449,1,1V24.956z"
+                            />
                         </svg>
                     </div>
                 </div>
